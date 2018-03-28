695710bdc33b4ef7f8ca3d748a75296d9e4869c6
taotsetung <taotsetung@gmail.com>
2018-03-28 18:00:28 +0200

    Updated to version 2.0
    - Now handles master xlsx files with multiple languages
    - two output modes
    - utf8 encoding

---
+++ GTT.Terminal/CommandLineOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace GTT.Terminal
+{
+    public class CommandLineOptions
+    {
+        [Value(0, MetaName = "File", Required=true, HelpText = "The file to convert. Can be any .loo file or a .xlsx file in the format of the multi-language master sheet.")]
+        public string File { get; set; }
+
+        [Option(Default = false, Required = false, HelpText = "Creates correctly named files for each language.")]
+        public bool CreateLanguageFiles { get; set; }
+
+        [Option(Default = null, Required = false, HelpText = "Optional, an xlsx file containing format information for the tool.")]
+        public string FormatFile { get; set; }
+    }
+}
--- GTT.Terminal/Program.cs
+++ GTT.Terminal/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using CommandLine;
 using GTT.Common;
 
 namespace GTT.Terminal
@@ -12,51 +14,52 @@ namespace GTT.Terminal
 
         private static void Main(string[] args)
         {
+            Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(opts => Run(opts));
+        }
+
+        private static void Run(CommandLineOptions options)
+        {
             try
             {
-                if (args.Length <= 1) //If no file was supplied exit.
-                {
-                    PrintHelp();
-                }
-                else
-                {
-                    var file = args[0];
-                    var codePage = int.Parse(args[1]);
                     var looParser = new LooParser();
                     var xlsxParser = new XlsxParser();
-                    switch (file)
+                    switch (options.File)
                     {
-                        case var xlsxFile when Path.GetExtension(file)
+                        case var xlsxFile when Path.GetExtension(options.File)
                             .Equals(XlsxExtension, StringComparison.CurrentCultureIgnoreCase)
                         : //if xlsxl file -> convert to loo
-                            var xlsxLines = xlsxParser.LoadXlsFile(xlsxFile, args.Length > 2 ? args[2] : null);
-                            looParser.SaveLooFile(xlsxLines, Path.GetFileNameWithoutExtension(xlsxFile) + LooExtension);
+                            foreach(var language in Enum.GetValues(typeof(ParserLanguage)).Cast<ParserLanguage>())
+                            {
+                                Console.WriteLine($"Preparing loo file for {language} language.");
+                                var xlsxLines = xlsxParser.LoadXlsFile(xlsxFile, options.FormatFile, language);
+                                Console.WriteLine($"{xlsxLines.Count} keys found for {language} language.");
+                                var looFilename = options.CreateLanguageFiles ? $"locdirect_{language.ToString().ToLower()}{LooExtension}" : $"{((ParserLanguageShort) language).ToString().ToLower()}\\locdirect_english{LooExtension}";
+                                var looDirectory = Path.GetDirectoryName(looFilename);
+                                if(!string.IsNullOrWhiteSpace(looDirectory) && !Directory.Exists(looDirectory))
+                                    Directory.CreateDirectory(looDirectory);
+                                looParser.SaveLooFile(xlsxLines, looFilename);
+                                Console.WriteLine($"Saved {looFilename} for {language} language.");
+                            }
+                            
                             break;
-                        case var looFile when Path.GetExtension(file)
+                        case var looFile when Path.GetExtension(options.File)
                             .Equals(LooExtension, StringComparison.CurrentCultureIgnoreCase)
                         : //if loo file -> convert to xlsx 
+                            Console.WriteLine("Converting .loo file to .xslx");
                             var looLines = looParser.LoadLooFile(looFile);
-                            xlsxParser.SaveXlsFile(looLines, Path.GetFileNameWithoutExtension(looFile) + XlsxExtension);
+                            var xlsxFilename = Path.GetFileNameWithoutExtension(looFile) + XlsxExtension;
+                            xlsxParser.SaveXlsFile(looLines, xlsxFilename);
+                            Console.WriteLine($"Saved {xlsxFilename}.");
                             break;
                         default:
-                            PrintHelp();
+                            Console.WriteLine("Invalid file type supplied. Only .loo and .xlsx are valid.");
                             break;
                     }
-                }
             }
             catch (Exception e)
             {
-                PrintHelp();
-                throw e;
+                Console.WriteLine("An error occured: {0}", e);
             }
-        }
-
-        private static void PrintHelp()
-        {
-            Console.WriteLine(Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName) +
-                              " <file> [FormatFile]");
-            Console.WriteLine("file - the file to convert");
-            Console.WriteLine("FormatFile - Optional, an xlsx file containing format information for the tool.");
         }
     }
 }
