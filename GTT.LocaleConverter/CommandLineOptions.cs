a37946321e980a980f44aec47bb0f782f0b2f8b1
taotsetung <taotsetung@gmail.com>
2018-05-10 15:21:23 +0200

    added a locale converter for various experiments

---
+++ GTT.LocaleConverter/CommandLineOptions.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace GTT.LocaleConverter
+{
+    public class CommandLineOptions
+    {
+        [Value(0, MetaName = "File", Required = true,
+            HelpText = "The file to convert. Can be any file that matches the given source encoding.")]
+        public string File { get; set; }
+
+        [Option('s', Default = "utf-8", Required = true, 
+            HelpText = "The original encoding of the file. Can be UTF-8 or an ANSI code page.")]
+        public string SourceEncoding { get; set; }
+
+        [Option('t', Default = "1251", Required = true,
+            HelpText = "The new encoding of the file. Can be UTF-8 or an ANSI code page")]
+        public string TargetEncoding { get; set; }
+    }
+}
---
+++ GTT.LocaleConverter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using CommandLine;
+
+namespace GTT.LocaleConverter
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(opts => Run(opts));
+        }
+
+        private static void Run(CommandLineOptions options)
+        {
+            try
+            {
+                var sourceEncoding = GetEncoding(options.SourceEncoding);
+                var targetEncoding = GetEncoding(options.TargetEncoding);
+
+                var text = File.ReadAllText(options.File, sourceEncoding);
+                File.WriteAllText(options.File, text, targetEncoding);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occured: {0}", e);
+            }
+        }
+
+        private static Encoding GetEncoding(string name)
+        {
+            if (name.Equals("utf-8", StringComparison.InvariantCultureIgnoreCase) ||
+                name.Equals("utf8", StringComparison.InvariantCultureIgnoreCase))
+                return new UTF8Encoding(false); //Guild never uses BOM
+
+            //Assume code page. Not very safe but we only need this for now.
+            return Encoding.GetEncoding(int.Parse(name));
+        }
+    }
+}
