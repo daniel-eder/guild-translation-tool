049772583bc03a50cf4e50b60d2b8f5abc4c1f53
taotsetung <taotsetung@gmail.com>
2018-07-27 12:59:51 +0200

    added loo importer to import external translations

---
+++ GTT.ImportLoo/CommandLineOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace GTT.ImportLoo
+{
+    public class CommandLineOptions
+    {
+        [Value(0, MetaName = "LooFile", Required=true, HelpText = "The source loo file.")]
+        public string LooFile { get; set; }
+
+        [Value(1, MetaName = "SheetFile", Required=true, HelpText = "The target translation sheet file.")]
+        public string SheetFile { get; set; }
+
+        [Option(Default = null, Required = true, HelpText = "The language colum to insert into")]
+        public string Language {get; set;}
+    }
+}
---
+++ GTT.ImportLoo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+using CommandLine;
+using GTT.Common;
+
+namespace GTT.ImportLoo
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(opts => Run(opts));
+        }
+
+        private static void Run(CommandLineOptions options)
+        {
+            try
+            {
+               //Load XLS FIl 
+               //Load Loo File 
+               //for each loo key, insert isert text into xls if key exists too
+               
+               var looParser = new LooParser();
+               var workbook = new XLWorkbook(options.SheetFile);
+
+                var worksheet = workbook.Worksheets.Worksheet(1);
+                var language = (ParserLanguage)Enum.Parse(typeof(ParserLanguage), options.Language, true);  
+
+                var looLines = looParser.LoadLooFile(options.LooFile);
+                foreach(var line in looLines)
+                {
+                    var cell = worksheet.Columns(1, 1).CellsUsed().Where((IXLCell c )=> c.GetString() == line.Item1).FirstOrDefault();
+                    if(cell != null)
+                    {
+                        cell.WorksheetRow().Cell((int) language).SetValue(line.Item2);
+                    }
+
+                }
+                workbook.Save();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occured: {0}", e);
+            }
+        }
+    }
+}
